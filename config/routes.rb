7f307f3d3119a61755603ff97a5c9062847153b1
jin521 <autumn.qiujin@gmail.com>
2021-06-26 22:40:20 +1000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :results
+  resources :news
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
